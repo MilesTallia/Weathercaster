7fedf2b83f8242c18abac849d8d22607027fbf08
MilesTallia <mot7506@g.rit.edu>
2023-01-25 09:39:32 -0500

    dialog window and location get seem to work

--- Weathercaster.js
+++ Weathercaster.js
@@ -43,17 +43,21 @@ function display_date() { // sets up date
 
 var x = document.getElementById("demo");
 
+function closeDialog() {
+    document.getElementById("locationAsk").close();
+}
+
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
 }
 
 function weather() {
